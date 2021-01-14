cf0298304ac0bba0dce4d1ef7c9344f013b26562
foxamanda2 <fox.amanda2@gmail.com>
2021-01-14 18:40:10 -0500

    Working Deposit and View

--- Program.cs
+++ Program.cs
@@ -1,12 +1,143 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Transactions
     {
-      Console.WriteLine("Welcome to C#");
+        public string Account { get; set; }
+        public string DepOrWith { get; set; }
+        public int Amount { get; set; }
+
+        public DateTime TransactionTime;
     }
-  }
+    class Program
+    {
+
+        static void Greeting(string prompt)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Welcome to the First Bank of SunCoast");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+        static void Main(string[] args)
+        {
+
+            Greeting("Welcome to the First Bank of SunCoast");
+
+            var transactions = new List<Transactions>()
+            {
+              new Transactions
+              {
+                Account="Savings",
+                DepOrWith="Deposit",
+                Amount=45,
+                TransactionTime=DateTime.Now,
+              },
+
+              new Transactions()
+              {
+                Account="Checking",
+                DepOrWith="Deposit",
+                Amount=150,
+                TransactionTime=DateTime.Now,
+              },
+
+              new Transactions()
+              {
+                Account="Checking",
+                DepOrWith="Withdraw",
+                Amount=30,
+                TransactionTime=DateTime.Now,
+              },
+              new Transactions()
+              {
+                Account="Savings",
+                DepOrWith="Withdraw",
+                Amount=5,
+                TransactionTime=DateTime.Now,
+              }
+
+            };
+            var userQuit = false;
+            while (userQuit == false)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Menu Options:");
+                Console.WriteLine("View");
+                Console.WriteLine("Deposit");
+                Console.WriteLine("Withdraw");
+                Console.WriteLine("Quit");
+
+                var userInput = Console.ReadLine().ToLower().Trim();
+
+                if (userInput == "view")
+                {
+                    Console.Write("Would you like to view your Savings or Checking History?");
+                    var SorC = Console.ReadLine().ToLower();
+
+                    if (SorC == "savings" || SorC == "s")
+                    {
+                        var savings = transactions.Where(savings => savings.Account.ToLower() == "savings");
+                        foreach (var save in savings)
+                        {
+                            Console.WriteLine($"Your Transaction History: {save.Account}, {save.DepOrWith}, {save.Amount} at {save.TransactionTime}");
+                        }
+                    }
+                    if (SorC == "checking" || SorC == "c")
+                    {
+                        var checking = transactions.Where(checking => checking.Account.ToLower() == "checking");
+                        foreach (var check in checking)
+                        {
+                            Console.WriteLine($"Your Transaction History: {check.Account}, {check.DepOrWith}, {check.Amount} at {check.TransactionTime}");
+                        }
+                    }
+
+                }
+
+                if (userInput == "deposit")
+                {
+                    Console.Write("Would you like to deposit into your checkings or savings? ");
+                    var depositAccount = Console.ReadLine();
+
+                    Console.Write("How much would you like to deposit? ");
+                    var depositAmount = Console.ReadLine();
+
+
+                    var newtransaction = new Transactions();
+                    newtransaction.Account = depositAccount;
+                    newtransaction.Amount = int.Parse(depositAmount);
+                    newtransaction.DepOrWith = "Deposit";
+                    newtransaction.TransactionTime = DateTime.Now;
+
+                    transactions.Add(newtransaction);
+
+                    Console.WriteLine($"Thank you! Your deposit of {depositAmount} has been applied to your {depositAccount} account.");
+
+                }
+
+
+
+
+
+                if (userInput == "quit")
+                {
+                    userQuit = true;
+                }
+            }
+
+
+
+
+
+
+
+            Greeting("Thank you for your buisness");
+
+        }
+    }
 }
+
--- README.md
+++ README.md
@@ -15,19 +15,33 @@
   --Option to view checkings and savings
 - After each transaction write them to the file to save
 
+C R U D
+
+Create: Add new transaction
+Read: Get lists of transactions that are in the history. Get balance of savings and checking
+Update: Update information about a transaction (checking/Savings)
+Delete: Remove a balance from transaction(withdraw/deposit)
+
 ##Examples
 
 - If a customer withdraws $40, but they only have $20 the program should not withdraw and say "Insufficient funds"
 - Customer should not be able to deposit -$50 into any account.
 - Customer should be able to deposit $20 into checkings or savings account
-- If a customer asks to make a withdraw from savings then the savings account should be displayed to withdraw from
+- If a customer asks to make a withdraw from savings then the savings account should be displayed to withdraw from.
+- If a customer asks for their transaction history then their whole list of transactions should print out.
+- When a customer asks for their balance they should be returned with their total balance.
 
 ##Data
 
-- Class BankAccount
-  --Name-String
-  --Savings-int
-  --Checkings-int
+- Class transaction
+  --Amount-int (The price you want)
+  --Savings/Checking-string Type ()
+  --Transfer--string
+
+  - new list <transaction> csv
+
+  --Behaviors that a property can do limited to transaction.
+  --Behavior to describe the transaction
 
 > var customerChoice= false;
 > While (customerChoice==false);
@@ -37,13 +51,20 @@
   --Deposit to checkings
   --withdraw from savings
   --Withdraw from checkings
-  --View Checkings and Savings
+  --View Checking and Savings history
   --if (customerChoice=="quit"){
-  customerChoise=true;
+  customerChoice=true;
   }
   --Quit Option
 
-  Algorithm
+  ##Algorithm
+
+  class Transaction
+
+  - Class transaction
+    --Amount-int (The price you want)
+    --Savings/Checking-string Type ()
+    --Transfer--string
 
   1. Welcome to the bank
      var userChoice=false;
@@ -54,6 +75,9 @@
 
   2. Menu Options ("Would you like to?");
      -View
+     --Savings
+     --Checking
+     -Balance
      --Savings
      --Checking
      -Deposit
@@ -68,23 +92,30 @@
   -Write a question (savings or checking)
   -If question answer is savings
   --find savings
-  --print savings
+  --print Transaction history
   -If question answer is checking
   --find checking
   --print checking
+  --print Transaction history
 
-  if (userChoice==Deposit)
+  if (userChoice==Deposit) ADD
   -Write a question (savings or checking)
-  -If question answer is savings
-  --Find savings
-  --Ask how much they would like to deposit (int.Parce)
-  --Add that amount from savings
-  -If question answer is checking
-  --Find checking
-  --Ask how much they would like to deposit (int.Parce)
-  --Add that amount from checking
+  --var newAccount=Answer (savings or checkings)
+  -How much do you want to add?
+  --var newAmount= answer (How much)
+  --var newTransfer=deposit (deposit or withdraw)
 
-  if (userChoice==withdraw)
+  var newtransaction= new Transaction()
+
+  newtransaction.Account=newAccount
+  newtransaction.Amount=newAmount
+  newtransaction.Transfer=newTransfer
+
+  transaction.Add(newtransaction)
+
+  Four lines of WriteLines
+
+  if (userChoice==withdraw) REMOVE
   -Write a question (savings or checking)
   -If question answer is savings
   --Find savings
@@ -92,7 +123,6 @@
   --subtract that amount from savings
   ---If that amount (savings<0)
   ---Write "insufficient funds"
-  ---return money back?
   -If question answer is checking
   --Find checking
   --Ask how much they would like to withdraw (int.Parce)
